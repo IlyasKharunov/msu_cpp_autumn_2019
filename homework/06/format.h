fed0f44cd4f831e96919271d08a4810c6d1257c1
IlyasKharunov <56125857+IlyasKharunov@users.noreply.github.com>
2020-01-18 22:10:01 +0300

    Add files via upload

--- homework/06/format.h
+++ homework/06/format.h
@@ -5,61 +5,17 @@
 #include<limits>
 #include<vector>
 
-int to_int(const std::string& s,size_t &pos) {
-	long long num = 0;
-	for (; pos < s.size(); pos++) {
-		if (s[pos] > '9' || s[pos] < '0') break;
-		num = num * 10 + s[pos] - '0';
-		if (num > std::numeric_limits<int>::max()) break;
-	}
-	if (s[pos] != '}') return -1;
-	else return num;
-}
-
-bool can_stream(...) {
-	return false;
-}
+bool can_stream(...);
 
 template<class T>
-bool can_stream(std::string& s, T&& t) {
-	typename decltype(std::cout << t)* = nullptr;
-	std::stringstream tmp_stream;
-	tmp_stream << t;
-	tmp_stream >> s;
-	return true;
-}
+bool can_stream(std::string& s, T&& t, typename decltype(std::cout << t)* = nullptr);
 
-void parse_args(std::vector<std::string>& args) {
-	return;
-}
+void parse_args(std::vector<std::string>& args);
 
 template<class T,class... ArgsT>
-void parse_args(std::vector<std::string>& args, T &&a, ArgsT&& ...arg) {
-	args.push_back("");
-	if (can_stream(args.back(),a)) {
-		parse_args(args, std::forward<ArgsT>(arg)...);
-	}
-	else throw (std::runtime_error("Cant output to stream"));
-}
-
-
-
+void parse_args(std::vector<std::string>& args, T &&a, ArgsT&& ...arg);
 
 template<class... ArgsT>
-std::string format(const std::string& s, ArgsT&&... arg) {
-	std::vector<std::string> args;
-	parse_args(args, std::forward<ArgsT>(arg)...);
-	std::string ans = "";
-	for (size_t i = 0; i < s.size(); i++) {
-		if (s[i] == '{') {
-			if (i == s.size() - 1) throw(std::runtime_error("too much {"));
-			int num = to_int(s, ++i);
-			if (num == -1) throw(std::runtime_error("Wrong input in brackets"));
-			if (num >= args.size()) throw(std::runtime_error("Not enough arguments"));
-			ans += args[num];
-		}
-		else if (s[i] == '}') throw(std::runtime_error("too much }"));
-		else ans += s[i];
-	}
-	return ans;
-}
+std::string format(const std::string& s, ArgsT&&... arg);
+
+#include "format.tpp"
